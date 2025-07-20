8049f9879ad9cd06730318476bc1a01534c00d66
Moh Eka Syafrino Nazhifan <bloodsuker18@gmail.com>
2025-07-20 15:09:25 +0700

    feat: add reconnect strategy

--- server/plugins/database/redis.ts
+++ server/plugins/database/redis.ts
@@ -4,7 +4,18 @@ import logger from "~/server/utils/logger";
 
 export default defineNitroPlugin(async (nitroApp) => {
     const client = createClient({
-        url: useRuntimeConfig().REDIS_URL as string
+        socket: {
+            connectTimeout: 10000,
+            reconnectStrategy: (retries) => {
+                if (retries > 5) {
+                    logger.error("Max retries reached for Redis connection");
+                    return new Error("Max retries reached");
+                }
+                logger.warn(`Redis connection retry attempt: ${retries}`);
+                return Math.min(retries * 1000, 5000); // Exponential backoff with a max delay of 5 seconds
+            }
+        },
+        url: useRuntimeConfig().REDIS_URL as string,
     });
     try {
         await client.connect();
